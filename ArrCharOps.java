d7510cdcb94614115b6b35c24c3169c8cc71ff25
Yaniv Siboni <yaniv.siboni@post.runi.ac.il>
2025-11-23 00:17:13 +0200

    done

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -177,8 +177,7 @@ public class ArrCharOps {
         for (int i = 0; i < Math.min(str1.length(), str2.length());i++){
             char c1 = Character.toLowerCase(str1.charAt(i));
         char c2 = Character.toLowerCase(str2.charAt(i));
-            if (c1 >= 'A' && c1 <= 'Z') c1 = (char)(c1 + 32);
-            if(c2 >= 'A' && c2 <= 'Z') c2 = (char)(c2 + 32);
+          
             
             if (c1 < c2) return -1;
             if (c1 > c2) return 1;
