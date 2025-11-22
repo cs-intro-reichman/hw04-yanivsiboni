e15b915d6e6be95d914f39d3841ca8038e8e754f
Yaniv Siboni <yaniv.siboni@post.runi.ac.il>
2025-11-22 18:06:29 +0200

    done

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -171,7 +171,7 @@ public class ArrCharOps {
         }
     
         for (int i = 0; i < str2.length(); i++){
-            if ((str2.charAt(i) < 'A' ||  str2.charAt(i) > 'Z') && (str2.charAt(i) < 'A' || str2.charAt(i) > 'z'))
+            if ((str2.charAt(i) < 'A' ||  str2.charAt(i) > 'Z') && (str2.charAt(i) < 'a' || str2.charAt(i) > 'z'))
                 return -2;
         }
         for (int i = 0; i < Math.min(str1.length(), str2.length());i++){
