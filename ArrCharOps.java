f4f816ecfc111dbe238f87e2022285bcc66256e6
Yaniv Siboni <yaniv.siboni@post.runi.ac.il>
2025-11-22 18:19:13 +0200

    done

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -165,15 +165,7 @@ public class ArrCharOps {
      */
     public static int compareTo(String str1, String str2) {
         
-        for (int i = 0; i < str1.length(); i++){
-            if ((str1.charAt(i) < 'A' || str1.charAt(i) > 'Z') && (str1.charAt(i) < 'a' || str1.charAt(i) > 'z'))
-            return -2;
-        }
-    
-        for (int i = 0; i < str2.length(); i++){
-            if ((str2.charAt(i) < 'A' ||  str2.charAt(i) > 'Z') && (str2.charAt(i) < 'a' || str2.charAt(i) > 'z'))
-                return -2;
-        }
+        
         for (int i = 0; i < Math.min(str1.length(), str2.length());i++){
     
             if (str1.charAt(i) < str2.charAt(i))
